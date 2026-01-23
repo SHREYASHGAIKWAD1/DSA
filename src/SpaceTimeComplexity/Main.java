dce69596ccacb49428c8576936e2d8c66fcb6b6c
Shreyash Gaikwad <shreyashgaikwad02367@gmail.com>
2026-01-23 14:14:41 +0530

    2nd commit

--- src/SpaceTimeComplexity/Main.java
+++ src/SpaceTimeComplexity/Main.java
@@ -1,11 +1,12 @@
 package SpaceTimeComplexity;
 
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         //BigO(1) - Constant
         int[] arr = new  int[] {1,2,3,4,5,6};
         System.out.println(arr[0]);
 
+        //Simple definition - The amount time taken or amount of space taken remains constant.
         //Length of array goes to one million items || one item - Doesn't matter, which is why time taken is constant,
         //hence O(1) time complexity.
         //Note - Really difficult to achieve constant time.
