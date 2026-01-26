2b8b65f478bd14842a944055fa21d7051fbc193e
Shreyash Gaikwad <shreyashgaikwad02367@gmail.com>
2026-01-26 22:21:25 +0530

    Commit 6

--- src/SpaceTimeComplexity/LogSTC.java
+++ src/SpaceTimeComplexity/LogSTC.java
@@ -2,6 +2,27 @@ package SpaceTimeComplexity;
 
 public class LogSTC {
     public static void main(String[] args) {
+        int[] arr = new int[]{2,5,10,20,40,60,70};
 
+//        for (int i = 0; i < arr.length; i++){
+//            if(arr[i] == 40) {
+//                System.out.println("Found the value 40 at index " + i);     //Inefficient solution in terms of Time C.
+//            }
+//        }
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end) {
+            int mid = (start + end) / 2;
+            if(arr[mid] == 40) {
+                System.out.println("[BS] Found element 40 at index " + mid);
+                break;
+            } else if (arr[mid] < 40) {
+                start = mid + 1;
+            } else  {
+                end = mid - 1;
+            }
+        }
     }
 }
