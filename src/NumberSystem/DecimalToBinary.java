f1a2471ad2acb575e992acaed2b04efd2e4f8f73
Shreyash Gaikwad <shreyashgaikwad02367@gmail.com>
2026-03-22 02:18:06 +0530

    Commit 12

--- src/NumberSystem/DecimalToBinary.java
+++ src/NumberSystem/DecimalToBinary.java
@@ -12,26 +12,26 @@ public class DecimalToBinary {
         this.num = num;
     }
 
-    public String decimalToBinary(int num){
-//        ArrayList<Integer> arr = new ArrayList<>();
-//        while (num!=0) {
-//            if(num%2==0){
-//                arr.addLast(0);
-//                num = num/2;
-//            } else {
-//                arr.addLast(1);
-//                num = num/2;
-//            }
-//        }
-//        return arr.reversed();
-
-        String result = "";
-        while(num > 0){
-            int rem = num % 2;
-            num = num / 2;
-            result = rem + result;
+    public List<Integer> decimalToBinary(int num){
+        ArrayList<Integer> arr = new ArrayList<>();
+        while (num != 0) {
+            if (num % 2 == 0) {
+                arr.addLast(0);
+                num = num / 2;
+            } else {
+                arr.addLast(1);
+                num = num / 2;
+            }
         }
-        return result;
+        return arr.reversed();
+
+//        String result = "";
+//        while(num > 0){
+//            int rem = num % 2;
+//            num = num / 2;
+//            result = rem + result;
+//        }
+//        return result;
     }
 
     public static void main(String[] args) {
