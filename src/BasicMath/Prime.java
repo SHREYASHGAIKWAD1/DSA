a2d812958d6ca8bb6ea34a05e31d4a201c33a08f
Shreyash Gaikwad <shreyashgaikwad02367@gmail.com>
2026-03-29 04:09:03 +0530

    Commit 14

--- src/BasicMath/Prime.java
+++ src/BasicMath/Prime.java
@@ -1,9 +1,11 @@
 package BasicMath;
 
+import java.util.Arrays;
+
 public class Prime {
 
     public static boolean isPrime(int n) {
-        for(int i=2; i*i <= n; i++){
+        for(int i=2; i < n; i++){
             if(n%i==0) {
                 return false;
             }
@@ -11,7 +13,38 @@ public class Prime {
         return true;
     }
 
-    public static void main(String[] args) {
+//    public static int countPrimes(int n) {
+//        int count = 0;
+//        for(int i=2; i < n; i++){
+//            if(isPrime(i)){
+//                count++;
+//            }
+//        }
+//        return count;
+//    }
 
+    //Sieve of Eratosthenes - Algorithm to find all prime to n in an efficient time
+    public static int countPrimes(int n) {
+        if (n <= 2) return 0;
+        boolean[] primes = new boolean[n + 1];
+        Arrays.fill(primes, true);
+        primes[0] = false;
+        primes[1] = false;
+
+        int count = 0;
+        for(int i = 2; i < n; i++) {
+            if(primes[i]) {
+                count++;
+                for(int j = 2*i; j < n; j = j + i) {
+                    primes[j] = false;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPrime(10));
+        System.out.println(countPrimes(10));
     }
 }
