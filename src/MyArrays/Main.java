1eb7fe567c2427a9ea9dbe9a2253d5d1b03e5016
Shreyash Gaikwad <shreyashgaikwad02367@gmail.com>
2026-03-30 22:07:45 +0530

    Commit 17

--- src/MyArrays/Main.java
+++ src/MyArrays/Main.java
@@ -1,6 +1,20 @@
 package MyArrays;
 
-public class Main {
-    //Primitive Array support
+import java.util.ArrayList;
 
+public class Main {
+
+    public static void main(String[] args) {
+        //Primitive Array support
+        //Arrays are static in size
+        int[] arr = new int[5];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        arr[3] = 4;
+        arr[4] = 5;
+
+        System.out.println(arr[1]);
+        System.out.println(arr);
+    }
 }
